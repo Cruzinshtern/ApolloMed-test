72054993ae7b9b8acd8b36912621583e20ebd7dd
Bohdan <bohdanmatv1@gmail.com>
2025-04-27 22:43:04 +0300

    basic navbar improved

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal, WritableSignal } from '@angular/core';
 import { InputComponent } from '../../shared/input/input.component';
 
 @Component({
   selector: 'app-navbar',
   imports: [InputComponent],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NavbarComponent {
+  showUserMenu: WritableSignal<boolean> = signal<boolean>(false)
 
+  toggleShowUserMenu() {
+    this.showUserMenu.update((currentValue) => !currentValue);
+  }
 }
