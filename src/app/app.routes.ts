c51cc7f8e458cb53e3d95491006ace0b1a976d43
Bohdan <bohdanmatv1@gmail.com>
2025-04-27 20:34:46 +0300

    basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./dashboard/dashboard.component').then(m => m.DashboardComponent),
+  },
+  {
+    path: 'form',
+    loadComponent: () => import('./form/form.component').then(m => m.FormComponent),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'dashboard'
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
+];
