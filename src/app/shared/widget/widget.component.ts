fd02401fa5838b3d872cab773c0953380dff4d69
Bohdan <bohdanmatv1@gmail.com>
2025-04-28 19:20:56 +0300

    bugfixes

--- src/app/shared/widget/widget.component.ts
+++ src/app/shared/widget/widget.component.ts
@@ -1,12 +1,14 @@
 import { Component, input, InputSignal } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'apollo-widget',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './widget.component.html',
   styleUrl: './widget.component.scss'
 })
 export class WidgetComponent {
   title: InputSignal<string | undefined> = input<string>()
+  chips: InputSignal<any[] | undefined> = input<any[]>()
   background: InputSignal<string> = input<string>('bg-white')
 }
